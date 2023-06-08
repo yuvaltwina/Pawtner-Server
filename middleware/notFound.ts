4ac461c919b423bdca5c5fb6ec44399b33405caf
yuval <wdvwdvwdv6@gmail.com>
2023-06-08 15:46:00 +0300

    deployment

--- middleware/notFound.ts
+++ middleware/notFound.ts
@@ -1,8 +1,8 @@
-import { Request, Response } from "express";
-import CustomError from "../errors/CustomError";
+import { Request, Response } from 'express';
+import CustomError from '../errors/CustomError';
 
 const notFound = (req: Request, res: Response) => {
-  return res.redirect(`http://localhost:5173`);
+  return res.redirect(`https://pawtner-front.vercel.app/`);
 
   // throw new CustomError(404, "Route does not exist");
 };
--- routes/MainRoute.ts
+++ routes/MainRoute.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { usersRouter } from './User';
 import { dogsRouter } from './Dog';
 const router = Router();
+
 //להוסיף מידלוור של בדיקה האם יש יוזר (דרך הקוקי)
 router.use('/user', usersRouter);
 router.use('/dog', dogsRouter);
