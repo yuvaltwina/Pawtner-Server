200de15bca4161349c212135fcc6c82d7a451b0f
yuval <wdvwdvwdv6@gmail.com>
2023-06-08 15:46:43 +0300

    deployment

--- middleware/notFound.ts
+++ middleware/notFound.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import CustomError from '../errors/CustomError';
 
 const notFound = (req: Request, res: Response) => {
+  console.log(1);
   return res.redirect(`https://pawtner-front.vercel.app/`);
 
   // throw new CustomError(404, "Route does not exist");
