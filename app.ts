39745f75567d9d2788edd13eae9cde4fde006c35
yuval <wdvwdvwdv6@gmail.com>
2023-06-25 18:31:09 +0300

    test3

--- app.ts
+++ app.ts
@@ -25,7 +25,7 @@ app.use(
 );
 
 app.use(cookieParser());
-app.use(express.json({ limit: '100mb' }));
+app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb' }));
 
 app.use('/', MainRoute);
