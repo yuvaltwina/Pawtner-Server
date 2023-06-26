9c2c272075a36bb443fd6d34e33953fb10a5e0a6
yuval <wdvwdvwdv6@gmail.com>
2023-06-26 13:05:34 +0300

    fixed port

--- app.ts
+++ app.ts
@@ -13,7 +13,7 @@ const connectDB = require('./db/connect');
 dotenv.config();
 
 const uri = process.env.MONGO_URI;
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const app = express();
 
@@ -27,7 +27,11 @@ app.use(
 app.use(cookieParser());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb' }));
-
+app.get('/home', (req, res) => {
+  res.status(200).json({
+    message: 'home route',
+  });
+});
 app.use('/', MainRoute);
 app.use(notFound);
 app.use(errorHandler);
