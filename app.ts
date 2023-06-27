d371bde3867b5778d56bb3c0f9ff3ad6d2dbcb7f
yuval <wdvwdvwdv6@gmail.com>
2023-06-27 11:46:24 +0300

    changed website url

--- app.ts
+++ app.ts
@@ -11,18 +11,6 @@ const connectDB = require('./db/connect');
 // להוסיף את כל הדברי אבטחה מפרוייקטים קודמים
 dotenv.config();
 
-//הכנסתי את זה לפה ישירות במקום האימפורט
-// const mongoose = require('mongoose');
-// const connectDB = (url: string) => {
-//   return mongoose.connect(url, {
-//     useNewUrlParser: true,
-//     dbName: 'Pawtner',
-//     useUnifiedTopology: true,
-//     // useCreateIndex: true,
-//     // useFindAndModify: false,
-//   });
-// };
-
 const uri = process.env.MONGO_URI;
 const port = process.env.PORT || 3000;
 
@@ -30,7 +18,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://pawtner-client.vercel.app',
     credentials: true, //access-control-allow-credentials:true
   })
 );
@@ -38,11 +26,6 @@ app.use(
 app.use(cookieParser());
 app.use(express.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }));
-app.get('/home', (req, res) => {
-  res.status(200).json({
-    message: 'home route',
-  });
-});
 app.use('/', MainRoute);
 app.use(notFound);
 app.use(errorHandler);
@@ -50,7 +33,7 @@ const start = async () => {
   try {
     await connectDB(uri as string);
     app.listen(port, () => {
-      console.log(`Server listening at http://localhost:${port}`);
+      console.log(`Server listening at port: ${port}`);
     });
   } catch (error: any) {
     throw new CustomError(500, error?.message || 'Internal server error');
