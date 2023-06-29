05ff63815c44650b8d584627a9abb8688dcffa57
yuval <wdvwdvwdv6@gmail.com>
2023-06-29 14:23:40 +0300

    change website url

--- utils/data/variables.ts
+++ utils/data/variables.ts
@@ -1,3 +1,2 @@
-export const WEBSITE_URL =
-  'https://pawtner-client-6q7jscvgt-yuvaltwina.vercel.app';
+export const WEBSITE_URL = 'https://pawtner-client.vercel.app';
 export const SERVER_URL = 'https://pawtner-server.onrender.com';
