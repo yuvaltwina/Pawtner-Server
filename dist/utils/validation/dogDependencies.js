ea0b19615ed8d50ec917485f5c5a645afa6b5771
yuval <wdvwdvwdv6@gmail.com>
2023-06-29 16:01:41 +0300

    change name limit

--- dist/utils/validation/dogDependencies.js
+++ dist/utils/validation/dogDependencies.js
@@ -7,8 +7,8 @@ const joi_1 = __importDefault(require("joi"));
 const dogBuiltInOptions_1 = require("../data/dogBuiltInOptions");
 const { ageValuesArray, cityValuesArray, sizeValuesArray, breedValuesArray, genderValuesArray, } = dogBuiltInOptions_1.dogBuiltInOptions;
 const IMAGE_MAX_SIZE = 5 * 1024 * 1024;
-const NAME_REGEX = /^(?=.*[a-zA-Z].*[a-zA-Z])[a-zA-Z\s]{2,12}$/;
-const ABOUT_REGEX = /^.{20,500}$/;
+const NAME_REGEX = /^(?=.*[a-zA-Z].*[a-zA-Z])[a-zA-Z\s]{2,18}$/;
+const ABOUT_REGEX = /^.{15,200}$/;
 const ALLOWED_IMAGE_TYPES_REGEX = /^data:image\/(jpeg|jpg|png|avif);base64,/;
 const dogNameSchema = joi_1.default.string().pattern(NAME_REGEX).required().messages({
     'string.pattern.base': 'The name must be between 2 and 18 characters and contain only letters.',
--- utils/data/variables.ts
+++ utils/data/variables.ts
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
 // export const WEBSITE_URL =
 //   'https://pawtner-client-6q7jscvgt-yuvaltwina.vercel.app';
 export const WEBSITE_URL = 'http://localhost:5173';
 export const SERVER_URL = 'http://localhost:3000';
 
 // export const SERVER_URL = 'https://pawtner-server.onrender.com';
-=======
-export const WEBSITE_URL = 'https://pawtner-client.vercel.app';
-export const SERVER_URL = 'https://pawtner-server.onrender.com';
-// export const WEBSITE_URL = 'http://localhost:5173';
-// export const SERVER_URL = 'http://localhost:3000';
->>>>>>> main
