ac6cffad20455dfc62f59d2e65cd64fd55e09caf
yuval <wdvwdvwdv6@gmail.com>
2023-07-02 14:11:08 +0300

    change age names and merge

--- dist/utils/data/variables.js
+++ dist/utils/data/variables.js
@@ -1,9 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.SERVER_URL = exports.WEBSITE_URL = void 0;
-// export const WEBSITE_URL =
-//   'https://pawtner-client-6q7jscvgt-yuvaltwina.vercel.app';
-exports.WEBSITE_URL = 'http://localhost:5173';
-exports.SERVER_URL = 'http://localhost:3000';
-// export const SERVER_URL = 'https://pawtner-server.onrender.com';
+exports.WEBSITE_URL = 'https://pawtner-client.vercel.app';
+exports.SERVER_URL = 'https://pawtner-server.onrender.com';
+// export const WEBSITE_URL = 'http://localhost:5173';
+// export const SERVER_URL = 'http://localhost:3000';
 //# sourceMappingURL=variables.js.map
