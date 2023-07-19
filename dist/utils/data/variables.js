35f9ea6231fb038b5e6066214e238efd3cb0c5e9
yuval <wdvwdvwdv6@gmail.com>
2023-07-19 11:48:39 +0300

    change pawtner route

--- dist/utils/data/variables.js
+++ dist/utils/data/variables.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.SERVER_URL = exports.WEBSITE_URL = void 0;
-// export const WEBSITE_URL = 'https://pawtner-client.vercel.app';
-// export const SERVER_URL = 'https://pawtner-server.onrender.com';
-exports.WEBSITE_URL = 'http://localhost:5173';
-exports.SERVER_URL = 'http://localhost:3000';
+exports.WEBSITE_URL = 'https://pawtner.vercel.app';
+exports.SERVER_URL = 'https://pawtner-server.onrender.com';
+// export const WEBSITE_URL = 'http://localhost:5173';
+// export const SERVER_URL = 'http://localhost:3000';
 //# sourceMappingURL=variables.js.map
