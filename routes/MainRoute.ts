e6c5ca2c73c5adbc9c50931c32c821e6b12d9e54
yuval <wdvwdvwdv6@gmail.com>
2023-07-20 10:53:57 +0300

    add home

--- routes/MainRoute.ts
+++ routes/MainRoute.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express';
 import { usersRouter } from './User';
 import { dogsRouter } from './Dog';
+import serverResponse from '../utils/serverResponse';
+
 const router = Router();
 router.use('/user', usersRouter);
 router.use('/dog', dogsRouter);
+router.use('/', () => {
+  serverResponse('Home');
+});
 export { router as MainRoute };
